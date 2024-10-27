0f6f55ff1d364f81e606b5a8a5f1e497a3dc0bab
Swapnil Sharma <swapnil.s@caratlane.com>
2024-10-27 12:45:58 +0100

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="home">
       {/* Video Background */}
       <video className="home-video-background" autoPlay loop muted>
-        <source src="https://videos.pexels.com/video-files/2763004/2763004-hd_1922_1080_25fps.mp4" type="video/mp4" />
+        <source src="https://videos.pexels.com/video-files/29098991/12572038_2560_1440_25fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
@@ -16,7 +16,7 @@ export default function Home() {
       <div className="home-flex">
         <div className="home-colored-box">
           <h1>Your Walls Deserve More Than Just Color</h1>
-          <p>Each photograph I capture is a moment of discovery, now available as high-quality prints for your home or office. From vibrant cityscapes to serene nature scenes, every image reflects my passion for storytelling through the lens. Find the perfect print to add character and inspiration to your space.</p>
+          <h3>Each photograph I capture is a moment of discovery, now available as high-quality prints for your home or office. From vibrant cityscapes to serene nature scenes, every image reflects my passion for storytelling through the lens. Find the perfect print to add character and inspiration to your space.</h3>
           <br />
           <button style={{ marginTop: '0vmin' }} className="button-13 cart-header" onClick={() => navigate('/collection')}>
             Shop Now
@@ -27,16 +27,19 @@ export default function Home() {
       {/* Additional Sections */}
       <div className="home-flex-two">
         <div className="home-icons">
+            <img className="home-icons-image" src="https://img.icons8.com/wired/64/FFFFFF/delivery.png"/>
             <h2>Shipping & Delivery</h2>
-            <p className="para-description">Chipoichipoi!! Free shipping on orders over ₹ 5000 (within India).</p>
+            <h3 className="para-description">Chipoichipoi!! Free shipping on orders over ₹ 5000 (within India).</h3>
         </div>
         <div className="home-icons" >
- <h2>Damaged item?</h2>
-            <p className="para-description">No worries! Contact our customer support team and we'll make it right</p>
+            <img className="home-icons-image" src="https://img.icons8.com/external-outline-juicy-fish/60/FFFFFF/external-damaged-cyber-crime-outline-outline-juicy-fish.png"/>
+            <h2>Damaged item?</h2>
+            <h3 className="para-description">No worries! Contact our customer support team and we'll make it right</h3>
         </div>
         <div className="home-icons">
+            <img className="home-icons-image" src="https://img.icons8.com/wired/64/FFFFFF/wrong-pincode.png"/>
             <h2>Made the wrong order?</h2>
-            <p className="para-description">Write us ASAP! We can correct your address/size in the first 24 hours :)</p>
+            <h3 className="para-description">Write us ASAP! We can correct your address/size in the first 24 hours :)</h3>
         </div>
       </div>
     </div>
